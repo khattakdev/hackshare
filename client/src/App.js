d078cf78cf6f00f0744d3f67b43d2a63518aae02
Drewbi <drewalexander986@gmail.com>
2020-07-14 16:52:13 +0800

    Refactor button

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,11 @@
 import React from 'react';
-import { useAuth0 } from "@auth0/auth0-react";
+import { useAuth0 } from '@auth0/auth0-react';
 import './App.css';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 import Profile from './pages/Profile';
 import Experts from './pages/Experts';
 import Learners from './pages/Learners';
 import Landing from './pages/Landing';
-
-const LoginButton = (props) => {
-  return (
-    <button onClick={props.onclick}>{props.children}</button>
-  )
-}
-
 
 const App = (props) => {
   const {
@@ -26,7 +19,7 @@ const App = (props) => {
   const handleLogin = () => loginWithRedirect()
     return (
       <div>
-        {isAuthenticated ? <LoginButton onclick={handleLogout}>Log out</LoginButton> : <LoginButton onclick={handleLogin}>Log in</LoginButton> }
+        {isAuthenticated ? <button onClick={handleLogout}>Log out</button> : <button onClick={handleLogin}>Log in</button> }
         { error && <div>Oops, Somethign went wrong... {JSON.stringify(error)}</div> }
         { user && user.name }
         { props.children }
