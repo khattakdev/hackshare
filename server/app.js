4f0135fc135d4fd4d4f0aed1ba001f28b5cf014f
Khattak <akkhattak65@gmail.com>
2020-07-14 21:06:12 +0500

    Uncomment useful Code

--- server/app.js
+++ server/app.js
@@ -9,17 +9,17 @@ const app = express();
 
 app.use(bodyParser.json());
 
-// app.use(
-//   jwt({
-//     secret: jwks.expressJwtSecret({
-//       cache: true,
-//       rateLimit: true,
-//       jwksRequestsPerMinute: 5,
-//       jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
-//     }),
-//     algorithms: ["RS256"],
-//   })
-// );
+app.use(
+  jwt({
+    secret: jwks.expressJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
+    }),
+    algorithms: ["RS256"],
+  })
+);
 
 app.use("/user", routes.user);
 app.use("/expertise", routes.expertise);
