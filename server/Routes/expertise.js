54fe0cc609d552d381604bddad19691d8294663d
Khattak <akkhattak65@gmail.com>
2020-07-14 20:04:04 +0500

    Add Remove/Update APIs

--- server/Routes/expertise.js
+++ server/Routes/expertise.js
@@ -3,5 +3,7 @@ const expertiseController = require("../controller/expertise");
 const router = express.Router();
 
 router.post("/add", expertiseController.addExpertise);
+router.post("/remove", expertiseController.removeExpertise);
+router.post("/update", expertiseController.updateExpertise);
 
 module.exports = router;
