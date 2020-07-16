f4678867836d13ba38f0ba3a85cabd2e3fbd7e0a
Drewbi <drewalexander986@gmail.com>
2020-07-16 14:00:54 +0800

    Adds svg landing handling

--- client/src/pages/Landing/index.js
+++ client/src/pages/Landing/index.js
@@ -1,12 +1,31 @@
 import React from "react";
-// import styles from './index.module.css'
+import styles from "./index.module.css";
+import { ReactComponent as Illustration1 } from "../../assets/illustrations/Landing-Shared.svg";
+
+const landingSections = [
+  {
+    header: "Teach to Learn, Learn to Teach",
+    body:
+      "Teaching something is the best way to truely understand it. HackShare helps you find people to learn from and teach. Expand your expertise today.",
+    button: {
+      clickaction: "",
+      text: "Get Started",
+    },
+    illustrationComponent: Illustration1,
+  },
+];
+
+const renderSections = landingSections.map((section, i) => {
+  const Illustration = section.illustrationComponent;
+  return (
+    <div key={i}>
+      <Illustration className={styles.illustration}></Illustration>
+    </div>
+  );
+});
 
 const Landing = () => {
-  return (
-    <>
-      <h1>Landing</h1>
-    </>
-  );
+  return <>{renderSections}</>;
 };
 
 export default Landing;
