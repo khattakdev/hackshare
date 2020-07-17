589b058aad6f2994ed96f6a450a59556b5f0b520
Drewbi <drewalexander986@gmail.com>
2020-07-17 17:36:27 +0800

    Change axios base url back

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  // baseURL: "https://mlhhackshare.herokuapp.com/",
-  baseURL: "http://localhost:8080/",
+  baseURL: "https://mlhhackshare.herokuapp.com/",
 });
 
 export default axiosInstance;
